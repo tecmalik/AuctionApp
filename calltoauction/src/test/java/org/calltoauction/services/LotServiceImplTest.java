00ba7781de476d2c7a03c46025e909c252d31dff
tecmalik <abdulmalikojo2@gmail.com>
2025-04-19 23:49:32 +0100

    commoting test

---
+++ calltoauction/src/test/java/org/calltoauction/services/LotServiceImplTest.java
@@ -0,0 +1,15 @@
+package org.calltoauction.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class LotServiceImplTest {
+    @Test
+    public void A_UserCanRegisterTest() {
+
+    }
+
+
+
+}
---
+++ calltoauction/src/test/java/org/calltoauction/services/UserServiceImplTest.java
@@ -0,0 +1,13 @@
+package org.calltoauction.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+@SpringBootTest
+class UserServiceImplTest {
+    @Test
+    public void A_UserCanRegisterTest() {
+
+    }
+
+}
