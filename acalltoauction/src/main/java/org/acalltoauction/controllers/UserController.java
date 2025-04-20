02f2cb77feca7083c0675ca46e8bf1c61c193a34
tecmalik <abdulmalikojo2@gmail.com>
2025-04-20 18:45:23 +0100

    commmiting flapdoodle tests configuration & controllers

--- acalltoauction/src/main/java/org/acalltoauction/controllers/UserController.java
+++ acalltoauction/src/main/java/org/acalltoauction/controllers/UserController.java
@@ -1,8 +1,26 @@
 package org.acalltoauction.controllers;
 
+import jakarta.validation.Valid;
+import org.acalltoauction.dto.requests.UserSignUpRequest;
+import org.acalltoauction.services.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+    @Autowired
+    private UserServiceImpl userServiceImpl;
+    @PostMapping("/Signup/users")
+    public ResponseEntity<?> signUp(@Valid @RequestBody UserSignUpRequest patientSignUpRequest){
+        try {
+            return userServiceImpl.signUp(patientSignUpRequest);
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 
 }
