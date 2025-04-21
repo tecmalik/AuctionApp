7237f4f422ad2a51df52cb0bbb872212e78571bd
tecmalik <abdulmalikojo2@gmail.com>
2025-04-21 14:08:33 +0100

    commiting spring boot app

--- acalltoauction/src/main/java/org/acalltoauction/data/repository/LotRepository.java
+++ acalltoauction/src/main/java/org/acalltoauction/data/repository/LotRepository.java
@@ -2,9 +2,10 @@ package org.acalltoauction.data.repository;
 
 import org.acalltoauction.data.models.Lot;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 
-
+@Repository
 public interface LotRepository extends MongoRepository<Lot, String> {
 
 }
